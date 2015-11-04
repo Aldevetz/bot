b9439c3c7922fe41c27ef4a50712bae57805d773
Aldevetz <aldevetz@gmail.com>
2015-11-04 09:34:52 +0900

    気温を追加

--- weather.js
+++ weather.js
@@ -9,8 +9,15 @@ request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         parseString(body, function (err, result) {
 
-            var message = "みなさんおはようございます。Aldevetz天気予報です\n" + result.weatherforecast.pref[0].area[3].info[0]['$'].date + "\n" +
-                result.weatherforecast.pref[0].area[3].info[0].weather[0] + "\n" + result.weatherforecast.pref[0].area[3].info[0].weather_detail[0];
+            var day = result.weatherforecast.pref[0].area[3].info[0]['$'].date + "\n";
+            var weather = result.weatherforecast.pref[0].area[3].info[0].weather[0] + "\n";
+            var detail = result.weatherforecast.pref[0].area[3].info[0].weather_detail[0] + "\n";
+            var max = "最高気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[0]._ + "度\n";
+            var min = "最低気温は" + result.weatherforecast.pref[0].area[3].info[0].temperature[0].range[1]._ + "度です";
+
+            var message = "みなさんおはようございます。Aldevetz天気予報です\n" + day + weather + detail + max + min;
+
+            console.log(message);
 
             var options = {
                 hostname: 'typetalk.in',
